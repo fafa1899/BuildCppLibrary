70017cb7f8e12a930de75c87b90e40f7b153eb04
chenglei <chenglei@egova.com.cn>
2024-07-30 15:34:28 +0800

    [A]GDAL编译脚本

---
+++ Windows/BuildGDAL.ps1
@@ -0,0 +1,55 @@
+param( 
+    [string]$SourceAddress = "https://github.com/OSGeo/gdal/releases/download/v3.8.5/gdal385.zip",
+    [string]$SourceZipPath = "../Source/gdal385.zip",
+    [string]$SourceLocalPath = "./gdal-3.8.5",
+    [string]$Generator = "Visual Studio 16 2019",
+    [string]$MSBuild = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe",
+    [string]$InstallDir = "$env:GISBasic"  
+)
+
+if (!(Test-Path $SourceLocalPath)) {
+    if (!(Test-Path $SourceZipPath)) {
+        #下载源代码
+        Write-Output "Download Zip..."
+        Invoke-WebRequest -Uri $SourceAddress -OutFile $SourceZipPath
+    }
+
+    # 解压缩 ZIP 文件   
+    Write-Output "Unzip Source..."
+    Expand-Archive -Path $SourceZipPath -DestinationPath "./"
+}
+
+# 清除旧的构建目录
+$BuildDir = $SourceLocalPath + "/build"  
+if (Test-Path $BuildDir) {
+    Remove-Item -Path $BuildDir -Recurse -Force
+}
+New-Item -ItemType Directory -Path $BuildDir
+
+# 转到构建目录
+Push-Location $BuildDir
+
+try {
+    # 配置CMake  
+    cmake .. -G "$Generator" -A x64 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_PREFIX_PATH="$env:GISBasic" -DCMAKE_INSTALL_PREFIX="$InstallDir" -DGDAL_USE_CURL=OFF -DBUILD_TESTING=OFF -DGDAL_USE_OPENSSL=OFF -DGDAL_USE_SPATIALITE=OFF -DGDAL_USE_MSSQL_NCLI=OFF
+
+    # 生成解决方案并编译
+    & $MSBuild ALL_BUILD.vcxproj /p:Configuration=RelWithDebInfo /p:Platform=x64 /m
+
+    # 安装库
+    & $MSBuild INSTALL.vcxproj /p:Configuration=RelWithDebInfo /p:Platform=x64 /m
+
+    # 复制符号库
+    $PdbFiles = @(        
+        "./RelWithDebInfo/gdal.pdb"
+    ) 
+    $SymbolDir = $InstallDir + "/symbol"
+    foreach ($file in $PdbFiles) {  
+        Write-Output $file
+        Copy-Item -Path $file -Destination $SymbolDir
+    }   
+}
+finally {
+    # 返回原始工作目录
+    Pop-Location
+}
